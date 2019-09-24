2f863910dca00248ef4a521b0c1e30be2b719206
Angel Antezana <a.antezana@empresasim.com>
2019-09-24 11:44:04 -0400

    Tipos de datos

---
+++ variables.js
@@ -0,0 +1,8 @@
+var disabled = true; //true or false
+console.log(disabled);
+var firtsName = "angel";
+console.log(firtsName);
+var greeting = "hello " + firtsName;
+console.log(greeting);
+var list = [3, 7];
+console.log(list);
